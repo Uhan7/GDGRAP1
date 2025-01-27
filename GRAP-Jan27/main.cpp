13126c25f0978c594d44a9774ea3ac559524d66c
Uhan7 <luckyfellowfella@gmail.com>
2025-01-28 00:55:58 +0800

    PIUT MY NAME

--- GRAP-Jan27/main.cpp
+++ GRAP-Jan27/main.cpp
@@ -13,7 +13,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 800, "el octagono triste", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "Johann Kristoff Naranjo", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
